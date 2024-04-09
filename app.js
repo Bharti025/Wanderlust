f9ecd71401f0d6dea8d4b64daf507e48226effcf
Delta-demo <bhartiwadhwa025@gmail.com>
2024-04-09 14:20:24 +0530

    add project files

--- app.js
+++ app.js
@@ -41,9 +41,9 @@ app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 app.engine("ejs",ejsMate);
-// app.get("/",(req,res)=>{
-// res.send("Hi I am working");
-// });
+app.get("/",(req,res)=>{
+res.send("Hi I am working");
+});
 
 const store=MongoStore.create({
   mongoUrl:dbUrl,
